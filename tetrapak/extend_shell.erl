5dfdd7afa98a111e1fe3382973db93b168a7b58e
Dmitry Russ <dr@travelping.com>
2012-08-22 11:33:50 +0200

    require build defore extend shell

--- tetrapak/extend_shell.erl
+++ tetrapak/extend_shell.erl
@@ -1,4 +1,5 @@
 -task({"build:extendshell", "build extend shell commands"}).
 
 run("build:extendshell", _) ->
+    tetrapak:require("build:erlang"),
     tetrapak_task_shell:extended_shell_build().
