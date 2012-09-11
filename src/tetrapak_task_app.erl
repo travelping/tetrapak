47538ec1163d22a9a508563560eb205ea11522db
Dmitry Russ <dr@travelping.com>
2012-09-11 19:56:46 +0200

    refactor a code / add tetrapak dir / add some small changes, that make it compilable with "tetrapak build"

--- src/tetrapak_task_app.erl
+++ src/tetrapak_task_app.erl
@@ -1,7 +1,6 @@
 -module(tetrapak_task_app).
 -export([run/2]).
 
-%% TODO: Possibility to get a new application name
 run("create:application", _) ->
     case init:get_argument(app) of
         {ok, [[Name]]} ->
@@ -12,28 +11,48 @@ run("create:application", _) ->
 
 create_app(Name) ->
     AtomName = list_to_atom(Name),
+    %% create dirs
     Path = tetrapak:path(Name),
-    assert_tuple(file:make_dir(Name)),
-    io:format("create: ~s~n", [Path]),
     Src = filename:join(Path, "src"),
-    io:format("create: ~s~n", [Src]),
-    assert_tuple(file:make_dir(Src)),
-    FileName = Src ++ "/" ++ Name ++ ".app.src",
-    case file:open(FileName, [write]) of
+    TetrapakDir = filename:join(Path, "tetrapak"),
+    [create_dir(Dir) || Dir <- [Path, Src, TetrapakDir]],
+
+    %% base name
+    BaseName = Src ++ "/" ++ Name,
+
+    %% create files
+
+    AppSrc = BaseName ++ ".app.src",
+    write_appsrc(AppSrc, AtomName),
+    ErlangMod = BaseName ++ ".erl",
+    write_erlmod(ErlangMod, AtomName).
+
+create_dir(Name) ->
+    assert_tuple(file:make_dir(Name)),
+    io:format("create: ~s~n", [Name]).
+
+write_appsrc(AppSrc, Name) ->
+    Fun = fun(File) ->
+                  io:format(File, "{application, ~p, [~n    {description, \"\"},
+    {vsn, [\"0.0\"]},
+    {applications, [kernel, stdlib]},
+    {registered, []},
+    {env, []}~n]}.", [Name])
+           end,
+    writer(Fun, AppSrc).
+
+write_erlmod(ErlMod, Name) ->
+    writer(fun(File) -> io:format(File, "-module(~p).", [Name]) end, ErlMod).
+
+writer(Fun, Path) ->
+    case file:open(Path, [write]) of
         {ok, File} ->
-            write_in(File, AtomName),
+            Fun(File),
             file:close(File),
-            io:format("create: ~s~n", [FileName]);
+            io:format("create: ~s~n", [Path]);
         Error ->
             assert_tuple(Error)
     end.
-
-write_in(File, Name) ->
-    io:format(File, "{application, ~p, [~n", [Name]),
-    io:format(File, "~n    {description, \"\"},
-    {applications, [kernel, stdlib]},
-    {registered, []},
-    {env, []}~n]}.").
 
 assert_tuple(Name) when is_tuple(Name) ->
     tetrapak:fail("Error: ~p", [Name]);
